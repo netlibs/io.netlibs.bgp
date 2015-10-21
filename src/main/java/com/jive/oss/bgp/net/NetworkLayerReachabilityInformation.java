e9871439983018050e4e1adbdcdc59ffca2de3c3
Theo Zourzouvillys <theo@jive.com>
2015-10-21 05:40:48 -0600

    remove unused Xml attributes

--- src/main/java/com/jive/oss/bgp/net/NetworkLayerReachabilityInformation.java
+++ src/main/java/com/jive/oss/bgp/net/NetworkLayerReachabilityInformation.java
@@ -19,27 +19,21 @@ package com.jive.oss.bgp.net;
 import java.io.Serializable;
 import java.net.InetAddress;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-
 import org.apache.commons.lang3.builder.CompareToBuilder;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
-
-import lombok.ToString;
 
 /**
  * This class models the generic network layer reachabibility information as defined in RFC 4271 and RFC 2858.
  *
  * The Network Layer Reachability Information (NLRI) is a variable length bit field.
  *
- * On the network layer it is encondig as a leading length (1 octet) and a varaiable number of of octets
- * each carrying an 8-bit part of the NLRI. The rightmost octet is filled up with trailing bits which a re ignored by the receiver and should
- * be set to 0 by the sender.
- * If the prefix length is zero then the number of trailing NLRI octets is zero as well.
+ * On the network layer it is encondig as a leading length (1 octet) and a varaiable number of of octets each carrying an 8-bit part of the
+ * NLRI. The rightmost octet is filled up with trailing bits which a re ignored by the receiver and should be set to 0 by the sender. If the
+ * prefix length is zero then the number of trailing NLRI octets is zero as well.
  *
- * Then number of octets need to carry the NLRI can be calculated from this formula: number of octets = (prefix length / 8) + (prefix length % 8 > 0 ? 1 : 0)
+ * Then number of octets need to carry the NLRI can be calculated from this formula: number of octets = (prefix length / 8) + (prefix length
+ * % 8 > 0 ? 1 : 0)
  *
  * NB: This class is also used to model the IPv4 withdrawn routes information in UPDATE (type 2) packets.
  *
@@ -47,10 +41,8 @@ import lombok.ToString;
  *
  */
 
-@ToString
-@XmlRootElement(name="nlri")
-@XmlAccessorType(XmlAccessType.FIELD)
-public class NetworkLayerReachabilityInformation implements Serializable, Comparable<NetworkLayerReachabilityInformation>  {
+public class NetworkLayerReachabilityInformation implements Serializable, Comparable<NetworkLayerReachabilityInformation>
+{
 
   /**
    *
@@ -60,68 +52,82 @@ public class NetworkLayerReachabilityInformation implements Serializable, Compar
   private int prefixLength;
   private byte[] prefix;
 
-  public NetworkLayerReachabilityInformation() {}
+  public NetworkLayerReachabilityInformation()
+  {
+  }
 
-  public NetworkLayerReachabilityInformation(final int prefixLength, final byte[] prefix) {
+  public NetworkLayerReachabilityInformation(final int prefixLength, final byte[] prefix)
+  {
     this.setPrefix(prefixLength, prefix);
   }
 
-  public NetworkLayerReachabilityInformation(final InetAddress source) {
+  public NetworkLayerReachabilityInformation(final InetAddress source)
+  {
     final byte[] raw = source.getAddress();
 
-    this.setPrefix(8*raw.length, raw);
+    this.setPrefix(8 * raw.length, raw);
   }
 
   /**
    * @return the prefixLength
    */
-  public int getPrefixLength() {
+  public int getPrefixLength()
+  {
     return this.prefixLength;
   }
 
   /**
    * @return the prefix
    */
-  public byte[] getPrefix() {
+  public byte[] getPrefix()
+  {
     return this.prefix;
   }
 
   /**
-   * set the prefix length and value in one step. The prefix value length in octets is checked against the prefix length
-   * and the number of octets calculated from the prefix length. The trailing bits are masked out to 0.
+   * set the prefix length and value in one step. The prefix value length in octets is checked against the prefix length and the number of
+   * octets calculated from the prefix length. The trailing bits are masked out to 0.
    *
    * @param prefixLength
    * @param prefix
    */
-  public void setPrefix(final int prefixLength, final byte[] prefix) {
+  public void setPrefix(final int prefixLength, final byte[] prefix)
+  {
     this.prefixLength = prefixLength;
     this.prefix = prefix;
 
-    if(prefix == null) {
-      if(prefixLength != 0)
+    if (prefix == null)
+    {
+      if (prefixLength != 0)
       {
         throw new IllegalArgumentException("cannot set null prefix if prefix length greater 0");
       }
-      this.prefix = new byte[] {0};
-    } else if(prefixLength == 0) {
-      this.prefix = new byte[] {0};
-    } else {
+      this.prefix = new byte[] { 0 };
+    }
+    else if (prefixLength == 0)
+    {
+      this.prefix = new byte[] { 0 };
+    }
+    else
+    {
       final int prefixSize = calculateOctetsForPrefixLength(this.prefixLength);
 
-      if(prefix.length != prefixSize)
+      if (prefix.length != prefixSize)
       {
         throw new IllegalArgumentException("expected a prefix with " + prefixSize + " octets but got " + prefix.length + " octets");
       }
 
       // skip masking unless prefix length > 0
-      if(prefixLength > 0) {
+      if (prefixLength > 0)
+      {
         // mask out trailing bits
-        final int trailingBits = ((8*prefixSize) - prefixLength);
+        final int trailingBits = ((8 * prefixSize) - prefixLength);
 
-        if(trailingBits > 0) {
-          for(int bit = 0; bit < trailingBits; bit++)
+        if (trailingBits > 0)
+        {
+          for (int bit = 0; bit < trailingBits; bit++)
           {
-            prefix[prefixSize-1] &= ~(1<<bit);
+            prefix[prefixSize - 1] &= ~(1 << bit);
           }
         }
       }
@@ -129,45 +135,54 @@ public class NetworkLayerReachabilityInformation implements Serializable, Compar
   }
 
   /**
-   * check if this instance if a prefix of the given other prefix. The criteria for being a prefix are:
-   * - The prefix length of the other NLRI is longer than this NLRI length
-   * - The prefix bits match up to the shorter prefix length
+   * check if this instance if a prefix of the given other prefix. The criteria for being a prefix are: - The prefix length of the other
+   * NLRI is longer than this NLRI length - The prefix bits match up to the shorter prefix length
    *
-   * If the other NLRI equals this NLRI then this NLRI is NOT a prefix of the other NLRI. If this NLRI has a
-   * zero-length prefix length it is considered to be a prefix of any other NLRI except the other NLRI also has a
-   * zero-length prefix length
+   * If the other NLRI equals this NLRI then this NLRI is NOT a prefix of the other NLRI. If this NLRI has a zero-length prefix length it is
+   * considered to be a prefix of any other NLRI except the other NLRI also has a zero-length prefix length
    *
    * @param other
    * @return
    */
-  public boolean isPrefixOf(final NetworkLayerReachabilityInformation other) {
+  public boolean isPrefixOf(final NetworkLayerReachabilityInformation other)
+  {
     boolean isPrefix = false;
 
-    if(this.prefixLength > 0) {
+    if (this.prefixLength > 0)
+    {
       if ((other.prefixLength > 0)
-          && (other.prefixLength > this.prefixLength)) {
+          && (other.prefixLength > this.prefixLength))
+      {
         final int byteLength = calculateOctetsForPrefixLength(this.prefixLength);
         boolean match = true;
 
         // test the full prefix octets
-        for (int i = 0; i < (byteLength - 1); i++) {
-          if (this.prefix[i] != other.prefix[i]) {
+        for (int i = 0; i < (byteLength - 1); i++)
+        {
+          if (this.prefix[i] != other.prefix[i])
+          {
             match = false;
             break;
           }
         }
 
         // prefix octets match, check remaining bits
-        if (match) {
+        if (match)
+        {
           final int bitsToCheck = this.prefixLength % 8;
 
-          if (bitsToCheck == 0) {
+          if (bitsToCheck == 0)
+          {
             match = (this.prefix[byteLength - 1] == other.prefix[byteLength - 1]);
-          } else {
-            for (int i = 0; i < bitsToCheck; i++) {
+          }
+          else
+          {
+            for (int i = 0; i < bitsToCheck; i++)
+            {
               final int mask = 1 << (7 - i);
 
-              if ((this.prefix[byteLength - 1] & mask) != (other.prefix[byteLength - 1] & mask)) {
+              if ((this.prefix[byteLength - 1] & mask) != (other.prefix[byteLength - 1] & mask))
+              {
                 match = false;
                 break;
               }
@@ -177,35 +192,43 @@ public class NetworkLayerReachabilityInformation implements Serializable, Compar
 
         isPrefix = match;
       }
-    } else {
+    }
+    else
+    {
       isPrefix = (other.prefixLength > 0);
     }
 
     return isPrefix;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   *
    * @see java.lang.Object#hashCode()
    */
   @Override
-  public int hashCode() {
+  public int hashCode()
+  {
     return (new HashCodeBuilder())
         .append(this.getPrefixLength())
         .append(this.getPrefix())
         .toHashCode();
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   *
    * @see java.lang.Object#equals(java.lang.Object)
    */
   @Override
-  public boolean equals(final Object obj) {
-    if(!(obj instanceof NetworkLayerReachabilityInformation))
+  public boolean equals(final Object obj)
+  {
+    if (!(obj instanceof NetworkLayerReachabilityInformation))
     {
       return false;
     }
 
-    final NetworkLayerReachabilityInformation o =(NetworkLayerReachabilityInformation)obj;
+    final NetworkLayerReachabilityInformation o = (NetworkLayerReachabilityInformation) obj;
 
     return (new EqualsBuilder())
         .append(this.getPrefixLength(), o.getPrefixLength())
@@ -214,42 +237,51 @@ public class NetworkLayerReachabilityInformation implements Serializable, Compar
   }
 
   @Override
-  public int compareTo(final NetworkLayerReachabilityInformation other) {
+  public int compareTo(final NetworkLayerReachabilityInformation other)
+  {
     final CompareToBuilder builder = new CompareToBuilder();
 
     builder.append(this.getPrefixLength(), other.getPrefixLength());
 
-    if(builder.toComparison() == 0) {
+    if (builder.toComparison() == 0)
+    {
       final int byteLen = calculateOctetsForPrefixLength(this.prefixLength);
       final int otherByteLen = calculateOctetsForPrefixLength(other.prefixLength);
       final int commonByteLen = (byteLen > otherByteLen) ? otherByteLen : byteLen;
       final int commonPrefixLen = (this.prefixLength > other.prefixLength) ? other.prefixLength : this.prefixLength;
 
-      for (int i = 0; i < (commonByteLen - 1); i++) {
+      for (int i = 0; i < (commonByteLen - 1); i++)
+      {
         builder.append(this.getPrefix()[i], other.getPrefix()[i]);
       }
 
-      if(builder.toComparison() == 0) {
-        final int bitsToCheck = commonPrefixLen% 8;
+      if (builder.toComparison() == 0)
+      {
+        final int bitsToCheck = commonPrefixLen % 8;
 
-        if (bitsToCheck == 0) {
-          if(commonByteLen > 0)
+        if (bitsToCheck == 0)
+        {
+          if (commonByteLen > 0)
           {
-            builder.append(this.getPrefix()[commonByteLen-1], other.getPrefix()[commonByteLen-1]);
+            builder.append(this.getPrefix()[commonByteLen - 1], other.getPrefix()[commonByteLen - 1]);
           }
-        } else {
-          for (int i = 0; i < bitsToCheck; i++) {
+        }
+        else
+        {
+          for (int i = 0; i < bitsToCheck; i++)
+          {
             final int mask = 1 << (7 - i);
 
             builder.append(this.getPrefix()[commonByteLen - 1] & mask, other.getPrefix()[commonByteLen - 1] & mask);
-            if(builder.toComparison() != 0)
+            if (builder.toComparison() != 0)
             {
               break;
             }
           }
         }
 
-        if(builder.toComparison() == 0) {
+        if (builder.toComparison() == 0)
+        {
           builder.append(this.getPrefixLength(), other.getPrefixLength());
         }
       }
@@ -259,19 +291,21 @@ public class NetworkLayerReachabilityInformation implements Serializable, Compar
     return builder.toComparison();
   }
 
-  public static final int calculateOctetsForPrefixLength(final int prefixLength) {
-    return (prefixLength / 8) + ((prefixLength % 8) > 0 ? 1 :0);
+  public static final int calculateOctetsForPrefixLength(final int prefixLength)
+  {
+    return (prefixLength / 8) + ((prefixLength % 8) > 0 ? 1 : 0);
   }
 
-  private static final char[] chars = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+  private static final char[] chars = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
   @Override
-  public String toString() {
+  public String toString()
+  {
     final StringBuilder builder = new StringBuilder();
 
     for (final byte element : this.prefix)
     {
-      builder.append(chars[(element/ 16) & 0x0f]);
+      builder.append(chars[(element / 16) & 0x0f]);
       builder.append(chars[(element % 16) & 0x0f]);
     }
     builder.append('/');
--- src/main/java/com/jive/oss/bgp/net/PathSegment.java
+++ src/main/java/com/jive/oss/bgp/net/PathSegment.java
@@ -4,145 +4,180 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-
 import org.apache.commons.lang3.builder.CompareToBuilder;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
-public class PathSegment implements Comparable<PathSegment> {
-	private ASType asType;
-	private List<Integer> ases = new LinkedList<Integer>(); 
-	private PathSegmentType pathSegmentType;
-	
-	public PathSegment() {}
-	
-	public PathSegment(ASType asType) {
-		this.asType = asType;
-	}
-	
-	public PathSegment(ASType asType, PathSegmentType pathSegmentType, int[] asArray) {
-		this(asType);
+public class PathSegment implements Comparable<PathSegment>
+{
 
-		this.pathSegmentType = pathSegmentType;
-		
-		if(asArray != null)
-			for(int as : asArray)
-				ases.add(as);
-	}
+  private ASType asType;
+  private List<Integer> ases = new LinkedList<Integer>();
+  private PathSegmentType pathSegmentType;
 
-	/**
-	 * @return the asType
-	 */
-	public ASType getAsType() {
-		return asType;
-	}
+  public PathSegment()
+  {
+  }
 
-	/**
-	 * @return the ases
-	 */
-	public List<Integer> getAses() {
-		return ases;
-	}
+  public PathSegment(final ASType asType)
+  {
+    this.asType = asType;
+  }
 
-	/**
-	 * @param ases the ases to set
-	 */
-	public void setAses(List<Integer> ases) {
-		if(ases != null)
-			this.ases = ases;
-		else
-			this.ases = new LinkedList<Integer>();
-	}
+  public PathSegment(final ASType asType, final PathSegmentType pathSegmentType, final int[] asArray)
+  {
+    this(asType);
 
-	/**
-	 * @return the type
-	 */
-	public PathSegmentType getPathSegmentType() {
-		return pathSegmentType;
-	}
+    this.pathSegmentType = pathSegmentType;
 
-	/**
-	 * @param type the type to set
-	 */
-	public void setPathSegmentType(PathSegmentType type) {
-		this.pathSegmentType = type;
-	}
+    if (asArray != null)
+    {
+      for (final int as : asArray)
+      {
+        this.ases.add(as);
+      }
+    }
+  }
 
-	@Override
-	public int compareTo(PathSegment o) {
-		CompareToBuilder builder = (new CompareToBuilder())
-				.append(getAsType(), o.getAsType())
-				.append(getPathSegmentType(), o.getPathSegmentType())
-				.append(getAses().size(), o.getAses().size());
-		
-		if(builder.toComparison() == 0) {
-			Iterator<Integer> lit = getAses().iterator();
-			Iterator<Integer> rit = o.getAses().iterator();
-			
-			while(lit.hasNext())
-				builder.append(lit.next(), rit.next());
-		}
-		
-		return builder.toComparison();
-	}
+  /**
+   * @return the asType
+   */
+  public ASType getAsType()
+  {
+    return this.asType;
+  }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		HashCodeBuilder builder = (new HashCodeBuilder())
-				.append(getAsType())
-				.append(getPathSegmentType());
-		Iterator<Integer> it = getAses().iterator();
-		
-		while(it.hasNext())
-			builder.append(it.next());
-		
-		return builder.toHashCode();
-	}
+  /**
+   * @return the ases
+   */
+  public List<Integer> getAses()
+  {
+    return this.ases;
+  }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if(!(obj instanceof PathSegment))
-			return false;
-		
-		PathSegment o = (PathSegment)obj;
-		
-		EqualsBuilder builder = (new EqualsBuilder())
-				.append(getAsType(), o.getAsType())
-				.append(getPathSegmentType(), o.getPathSegmentType())
-				.append(getAses().size(), o.getAses().size());
-		
-		if(builder.isEquals()) {
-			Iterator<Integer> lit = getAses().iterator();
-			Iterator<Integer> rit = o.getAses().iterator();
-			
-			while(lit.hasNext())
-				builder.append(lit.next(), rit.next());
-		}
-		
-		return builder.isEquals();
-	}
+  /**
+   * @param ases
+   *          the ases to set
+   */
+  public void setAses(final List<Integer> ases)
+  {
+    if (ases != null)
+    {
+      this.ases = ases;
+    }
+    else
+    {
+      this.ases = new LinkedList<Integer>();
+    }
+  }
 
-	@Override
-	public String toString() {
-		ToStringBuilder builder = new ToStringBuilder(this)
-			.append("asType", asType)
-			.append("pathSegmentType", pathSegmentType);
-		
-		for(int as : ases)
-			builder.append("as", as);
-		
-		return builder.toString();
-	}
+  /**
+   * @return the type
+   */
+  public PathSegmentType getPathSegmentType()
+  {
+    return this.pathSegmentType;
+  }
+
+  /**
+   * @param type
+   *          the type to set
+   */
+  public void setPathSegmentType(final PathSegmentType type)
+  {
+    this.pathSegmentType = type;
+  }
+
+  @Override
+  public int compareTo(final PathSegment o)
+  {
+    final CompareToBuilder builder = (new CompareToBuilder())
+        .append(this.getAsType(), o.getAsType())
+        .append(this.getPathSegmentType(), o.getPathSegmentType())
+        .append(this.getAses().size(), o.getAses().size());
+
+    if (builder.toComparison() == 0)
+    {
+      final Iterator<Integer> lit = this.getAses().iterator();
+      final Iterator<Integer> rit = o.getAses().iterator();
+
+      while (lit.hasNext())
+      {
+        builder.append(lit.next(), rit.next());
+      }
+    }
+
+    return builder.toComparison();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode()
+  {
+    final HashCodeBuilder builder = (new HashCodeBuilder())
+        .append(this.getAsType())
+        .append(this.getPathSegmentType());
+    final Iterator<Integer> it = this.getAses().iterator();
+
+    while (it.hasNext())
+    {
+      builder.append(it.next());
+    }
+
+    return builder.toHashCode();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(final Object obj)
+  {
+    if (!(obj instanceof PathSegment))
+    {
+      return false;
+    }
+
+    final PathSegment o = (PathSegment) obj;
+
+    final EqualsBuilder builder = (new EqualsBuilder())
+        .append(this.getAsType(), o.getAsType())
+        .append(this.getPathSegmentType(), o.getPathSegmentType())
+        .append(this.getAses().size(), o.getAses().size());
+
+    if (builder.isEquals())
+    {
+      final Iterator<Integer> lit = this.getAses().iterator();
+      final Iterator<Integer> rit = o.getAses().iterator();
+
+      while (lit.hasNext())
+      {
+        builder.append(lit.next(), rit.next());
+      }
+    }
+
+    return builder.isEquals();
+  }
+
+  @Override
+  public String toString()
+  {
+    final ToStringBuilder builder = new ToStringBuilder(this)
+        .append("asType", this.asType)
+        .append("pathSegmentType", this.pathSegmentType);
+
+    for (final int as : this.ases)
+    {
+      builder.append("as", as);
+    }
+
+    return builder.toString();
+  }
 }
